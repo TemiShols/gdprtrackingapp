43d486d545a728024aa38f6cb97dc6c52be6308c
Temitope <tpsolesi@gmail.com>
2023-09-18 22:39:44 +0100

    Update settings.py

--- gdpr/settings.py
+++ gdpr/settings.py
@@ -95,27 +95,24 @@ WSGI_APPLICATION = 'gdpr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+
 #   DATABASES = {
 #       'default': {
-#           'ENGINE': 'django.db.backends.sqlite3',
-#           'NAME': BASE_DIR / 'db.sqlite3',
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#           'NAME': os.getenv('DB_NAME'),
+#           'USER': os.getenv('DB_USER'),
+#           'PASSWORD': os.getenv('DB_PASSWORD'),
+#           'HOST': os.getenv('DB_HOST'),
+#           'PORT': os.getenv('DB_PORT'),
 #       }
-#   }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
-
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
